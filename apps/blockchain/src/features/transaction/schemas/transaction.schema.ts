1736aa680d1b20ea427b203bca5bda50159a9e18
Can Çevik <mazlumcancevik@gmail.com>
2023-11-03 19:19:04 +0300

    feat(bc): add transaction schema

---
+++ apps/blockchain/src/features/transaction/schemas/transaction.schema.ts
@@ -0,0 +1,44 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument } from 'mongoose'
+
+export type TransactionDocument = HydratedDocument<Transaction>
+
+@Schema({
+  collection: 'transaction-pool',
+  versionKey: false,
+  timestamps: {
+    updatedAt: false
+  }
+})
+export class Transaction {
+  @Prop({
+    type: String
+  })
+  public senderAddress: string
+
+  @Prop({
+    type: String,
+    required: true
+  })
+  public receiverAddress: string
+
+  @Prop({
+    type: Number,
+    required: true
+  })
+  public amount: number
+
+  @Prop({
+    type: String,
+    required: true
+  })
+  public signature: string
+
+  @Prop({
+    type: Number,
+    required: true
+  })
+  public timestamp: number
+}
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction)
--- apps/blockchain/src/features/transaction/transaction.module.ts
+++ apps/blockchain/src/features/transaction/transaction.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { TransactionController } from './controllers'
 import { TransactionService } from './services'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Transaction, TransactionSchema } from './schemas'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Transaction.name, schema: TransactionSchema }])],
   controllers: [TransactionController],
   providers: [TransactionService]
 })
