d35fc46a65e7ba888d449a7853150ae63dde50a6
Can Çevik <mazlumcancevik@gmail.com>
2023-11-03 16:33:02 +0300

    feat(bc): add base middlewares

--- apps/blockchain/src/main.ts
+++ apps/blockchain/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/app.module'
 import { Config, ENV } from './config'
 import { setupSwagger } from './setup-swagger'
+import { setupApp } from './setup-app'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
 
   const config = app.get<Config>(ENV)
 
+  setupApp(app)
   setupSwagger(app)
 
   await app.listen(config.PORT)
---
+++ apps/blockchain/src/setup-app.ts
@@ -0,0 +1,16 @@
+import helmet from 'helmet'
+import compression from 'compression'
+import mongoSanitize from 'express-mongo-sanitize'
+import { Config, ENV } from './config'
+import { INestApplication } from '@nestjs/common'
+
+export function setupApp(app: INestApplication): void {
+  const config = app.get<Config>(ENV)
+
+  app.enableCors()
+  app.setGlobalPrefix(config.GLOBAL_PREFIX)
+
+  app.use(helmet())
+  app.use(compression())
+  app.use(mongoSanitize())
+}
