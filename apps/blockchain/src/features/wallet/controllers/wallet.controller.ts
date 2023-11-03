dd13e5076a845a4af473e0caae8f2aa39e510471
Can Çevik <mazlumcancevik@gmail.com>
2023-11-03 17:41:45 +0300

    feat(bc): add create wallet action

--- apps/blockchain/src/features/wallet/controllers/wallet.controller.ts
+++ apps/blockchain/src/features/wallet/controllers/wallet.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { WalletService } from '../services'
+import { Message } from '@common/decorators'
+import { Wallet } from '../interfaces'
 
+@ApiTags('wallets')
 @Controller('wallets')
-export class WalletController {}
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+
+  @Post()
+  @Message('Wallet created successfully.')
+  public createWallet(): Wallet {
+    return this.walletService.createWallet()
+  }
+}
