5a5fe9ec4bce43110dc7b250027129ede0b13316
Can Çevik <mazlumcancevik@gmail.com>
2023-11-04 16:59:25 +0300

    fix(bc): add missing get blocks method

--- apps/blockchain/src/features/blockchain/services/blockchain.service.ts
+++ apps/blockchain/src/features/blockchain/services/blockchain.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { BlockRepository } from '../repositories'
+import { BlockDocument } from '../schemas'
 
 @Injectable()
-export class BlockchainService {}
+export class BlockchainService {
+  constructor(private readonly blockRepository: BlockRepository) {}
+
+  public async getBlocks(): Promise<BlockDocument[]> {
+    return this.blockRepository.find({})
+  }
+
+  public async getLastBlock(): Promise<BlockDocument> {
+    return this.blockRepository.findOne({}).sort({ _id: -1 }).limit(1)
+  }
+}
