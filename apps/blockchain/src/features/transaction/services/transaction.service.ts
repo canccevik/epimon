f6cced0f197bc5331e3ffba3d179b906efcb05e4
Can Çevik <mazlumcancevik@gmail.com>
2023-11-04 11:47:30 +0300

    feat(bc): add calculate hash method to transaction

--- apps/blockchain/src/features/transaction/services/transaction.service.ts
+++ apps/blockchain/src/features/transaction/services/transaction.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { Transaction } from '../schemas'
+import crypto from 'crypto'
 
 @Injectable()
-export class TransactionService {}
+export class TransactionService {
+  public calculateHash(transaction: Transaction): string {
+    return crypto
+      .createHash('sha256')
+      .update(
+        transaction.receiverAddress +
+          transaction.senderAddress +
+          transaction.timestamp +
+          transaction.amount
+      )
+      .digest('hex')
+  }
+}
