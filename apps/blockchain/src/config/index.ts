672d78b7add95433a38ffdbbe9d30a47ea5d1050
Can Çevik <mazlumcancevik@gmail.com>
2023-11-04 16:00:45 +0300

    feat(bc): add create genesis transaction method

--- apps/blockchain/src/config/index.ts
+++ apps/blockchain/src/config/index.ts
@@ -1,10 +1,11 @@
-import { port, str } from 'envalid'
+import { port, str, num } from 'envalid'
 import { Static, makeValidators } from 'nestjs-envalid'
 
 export const config = {
   PORT: port({ default: 3001 }),
   GLOBAL_PREFIX: str({ default: 'api' }),
-  DATABASE_URI: str()
+  DATABASE_URI: str(),
+  OWNER_WALLET_INITIAL_BALANCE: num({ default: 10000 })
 }
 
 export const validators = makeValidators(config)
--- apps/blockchain/src/features/transaction/services/transaction.service.ts
+++ apps/blockchain/src/features/transaction/services/transaction.service.ts
@@ -1,15 +1,19 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import { BadRequestException, Inject, Injectable } from '@nestjs/common'
 import { Transaction, TransactionDocument } from '../schemas'
 import crypto from 'crypto'
 import { TransactionRepository } from '../repositories'
 import { ec as EC } from 'elliptic'
 import { isPublicKeyValid } from '@common/utils'
+import { Config, ENV } from '@config/index'
 
 @Injectable()
 export class TransactionService {
   private readonly ec = new EC('secp256k1')
 
-  constructor(private readonly transactionRepository: TransactionRepository) {}
+  constructor(
+    @Inject(ENV) private readonly config: Config,
+    private readonly transactionRepository: TransactionRepository
+  ) {}
 
   public async getTransactionPool(): Promise<TransactionDocument[]> {
     return this.transactionRepository.find({})
@@ -59,5 +63,17 @@ export class TransactionService {
     const transactionHash = this.calculateHash(transaction)
     const signingKey = this.ec.keyFromPublic(transaction.senderAddress, 'hex')
     return signingKey.verify(transactionHash, transaction.signature)
+  }
+
+  public createGenesisTransaction(privateKey: string): Transaction {
+    const publicAddress = this.ec.keyFromPrivate(privateKey).getPublic('hex')
+
+    const genesisTransaction = new Transaction()
+    genesisTransaction.senderAddress = publicAddress
+    genesisTransaction.receiverAddress = null
+    genesisTransaction.amount = this.config.OWNER_WALLET_INITIAL_BALANCE
+    genesisTransaction.timestamp = 0
+
+    return genesisTransaction
   }
 }
