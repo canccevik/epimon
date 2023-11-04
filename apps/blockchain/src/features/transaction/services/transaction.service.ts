a65ff11ceb52d6491058b93856de60ede567f311
Can Çevik <mazlumcancevik@gmail.com>
2023-11-04 12:14:34 +0300

    feat(bc): add sign transaction method

--- apps/blockchain/src/features/transaction/services/transaction.service.ts
+++ apps/blockchain/src/features/transaction/services/transaction.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@nestjs/common'
-import { Transaction } from '../schemas'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { Transaction, TransactionDocument } from '../schemas'
 import crypto from 'crypto'
+import { TransactionRepository } from '../repositories'
+import { ec as EC } from 'elliptic'
+import { isPublicKeyValid } from '@common/utils'
 
 @Injectable()
 export class TransactionService {
+  private readonly ec = new EC('secp256k1')
+
+  constructor(private readonly transactionRepository: TransactionRepository) {}
+
+  public async getTransactionPool(): Promise<TransactionDocument[]> {
+    return this.transactionRepository.find({})
+  }
+
+  public async cleanTransactionPool(): Promise<void> {
+    await this.transactionRepository.deleteMany({})
+  }
+
   public calculateHash(transaction: Transaction): string {
     return crypto
       .createHash('sha256')
@@ -14,5 +29,25 @@ export class TransactionService {
           transaction.amount
       )
       .digest('hex')
+  }
+
+  public signTransaction(privateKey: string, transaction: Transaction): void {
+    const signingKey = this.ec.keyFromPrivate(privateKey)
+    const publicAddress = signingKey.getPublic('hex')
+
+    transaction.senderAddress = publicAddress
+    transaction.timestamp = Date.now()
+
+    if (!isPublicKeyValid(transaction.receiverAddress)) {
+      throw new BadRequestException('Receiver address is not valid.')
+    }
+
+    if (publicAddress === transaction.receiverAddress) {
+      throw new BadRequestException('You cannot make transactions on your own.')
+    }
+
+    const transactionHash = this.calculateHash(transaction)
+    const signature = signingKey.sign(transactionHash, 'base64')
+    transaction.signature = signature.toDER('hex')
   }
 }
