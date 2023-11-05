f342b5a4b00d040bf86cea3e5ad310731fb0c3cf
Can Çevik <mazlumcancevik@gmail.com>
2023-11-05 18:19:15 +0300

    feat(bc): add get chain action

--- apps/blockchain/src/features/blockchain/controllers/blockchain.controller.ts
+++ apps/blockchain/src/features/blockchain/controllers/blockchain.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
+import { BlockDocument } from '../schemas'
+import { BlockRepository } from '../repositories'
+import { Message } from '@common/decorators'
 
 @ApiTags('chain')
 @Controller('chain')
-export class BlockchainController {}
+export class BlockchainController {
+  constructor(private readonly blockRepository: BlockRepository) {}
+
+  @Get()
+  @Message('Chain fetched successfully.')
+  public async getChain(): Promise<BlockDocument[]> {
+    return this.blockRepository.find({})
+  }
+}
