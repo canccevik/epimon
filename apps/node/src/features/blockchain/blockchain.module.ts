62d4eb2a3ddd1fc05e419a7580c386982fb1e54c
Can Çevik <mazlumcancevik@gmail.com>
2023-11-14 17:31:27 +0300

    feat(node): add mine block method

--- apps/node/src/features/blockchain/blockchain.module.ts
+++ apps/node/src/features/blockchain/blockchain.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { BlockchainService } from './blockchain.service'
+import { WalletModule } from '@features/wallet/wallet.module'
 
 @Module({
+  imports: [WalletModule],
   providers: [BlockchainService],
   exports: [BlockchainService]
 })
--- apps/node/src/features/blockchain/blockchain.service.ts
+++ apps/node/src/features/blockchain/blockchain.service.ts
@@ -1,10 +1,14 @@
 import { Config, ENV } from '@config/index'
+import { WalletService } from '@features/wallet/wallet.service'
 import { Inject, Injectable, HttpStatus } from '@nestjs/common'
 import axios from 'axios'
 
 @Injectable()
 export class BlockchainService {
-  constructor(@Inject(ENV) private readonly config: Config) {}
+  constructor(
+    @Inject(ENV) private readonly config: Config,
+    private readonly walletService: WalletService
+  ) {}
 
   public async syncChainWithRoot(): Promise<void> {
     const { status } = await axios.post(this.config.LOCAL_API_URI + '/chain/sync')
@@ -13,5 +17,24 @@ export class BlockchainService {
       throw new Error('❌ Something went wrong while syncing the chain.')
     }
     console.info('✅ Chain synced with root chain successfully.')
+  }
+
+  public async mineBlock(): Promise<void> {
+    const minerWallet = await this.walletService.getAddressesFromSecretPhrase(
+      this.config.MINING_WALLET_SECRET_PHRASE
+    )
+
+    const mineEndpoint = this.config.LOCAL_API_URI + '/chain/mine'
+    const mineRequest = await axios.post(
+      mineEndpoint,
+      {},
+      { headers: { 'x-private-key': minerWallet.privateAddress } }
+    )
+
+    if (mineRequest.status !== HttpStatus.CREATED) return
+
+    console.info('✅ New block mined successfully.')
+    const balance = await this.walletService.getBalanceOfAddress(minerWallet.publicAddress)
+    console.info(`🟢 Your current balance is: ${balance}`)
   }
 }
