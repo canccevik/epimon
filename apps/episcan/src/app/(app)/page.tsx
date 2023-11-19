274ad86c42782002312a722f6c7a792dfe8a6b2c
Can Çevik <mazlumcancevik@gmail.com>
2023-11-19 20:12:50 +0300

    feat(es): add chain stats section

--- apps/episcan/src/app/(app)/page.tsx
+++ apps/episcan/src/app/(app)/page.tsx
@@ -1,9 +1,17 @@
+import { BadgeDollarSign, Cuboid, User2 } from 'lucide-react'
 import SearchBanner from './search-banner'
+import InfoCard from '@/components/info-card'
 
 export default function Home() {
   return (
-    <div className="flex p-10">
+    <div className="flex flex-col p-10 gap-y-10">
       <SearchBanner />
+
+      <div className="w-full flex justify-around mt-8">
+        <InfoCard title="$121.80" description="EPM Price" icon={<BadgeDollarSign size={30} />} />
+        <InfoCard title="33634430" description="Latest Block" icon={<Cuboid size={30} />} />
+        <InfoCard title="32" description="Validators" icon={<User2 size={30} />} />
+      </div>
     </div>
   )
 }
---
+++ apps/episcan/src/components/info-card.tsx
@@ -0,0 +1,20 @@
+interface ComponentProps {
+  icon: React.ReactNode
+  title: string
+  description: string
+}
+
+export default function InfoCard({ icon, title, description }: ComponentProps) {
+  return (
+    <div className="w-3/12 p-14 bg-white flex justify-center relative text-center rounded-lg border-gray-200 border">
+      <div className="w-[80px] h-[80px] bg-white rounded-full absolute text-center -top-10 grid place-items-center border-gray-200 border">
+        {icon}
+      </div>
+
+      <div>
+        <h1 className="text-4xl font-semibold tracking-wider">{title}</h1>
+        <p className="mt-3 font-medium text-lg">{description}</p>
+      </div>
+    </div>
+  )
+}
