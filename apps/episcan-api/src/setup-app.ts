fe58445c5e361f84961b2270fc73cb7a182296c0
Can Çevik <mazlumcancevik@gmail.com>
2023-11-23 16:46:57 +0300

    feat(esa): add base middlewares

--- apps/episcan-api/src/setup-app.ts
+++ apps/episcan-api/src/setup-app.ts
@@ -1,9 +1,16 @@
 import { INestApplication } from '@nestjs/common'
 import { Config, ENV } from './config'
+import helmet from 'helmet'
+import compression from 'compression'
+import mongoSanitize from 'express-mongo-sanitize'
 
 export function setupApp(app: INestApplication): void {
   const config = app.get<Config>(ENV)
 
   app.enableCors()
   app.setGlobalPrefix(config.GLOBAL_PREFIX)
+
+  app.use(helmet())
+  app.use(compression())
+  app.use(mongoSanitize())
 }
