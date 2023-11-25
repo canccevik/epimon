2f645af2fe201104766ce7c0d60e8502c349821a
Can Çevik <mazlumcancevik@gmail.com>
2023-11-25 23:00:51 +0300

    feat(esa): use validation pipe

--- apps/episcan-api/src/setup-app.ts
+++ apps/episcan-api/src/setup-app.ts
@@ -3,6 +3,7 @@ import { Config, ENV } from './config'
 import helmet from 'helmet'
 import compression from 'compression'
 import mongoSanitize from 'express-mongo-sanitize'
+import { ValidationPipe } from '@epimon/common'
 
 export function setupApp(app: INestApplication): void {
   const config = app.get<Config>(ENV)
@@ -13,4 +14,6 @@ export function setupApp(app: INestApplication): void {
   app.use(helmet())
   app.use(compression())
   app.use(mongoSanitize())
+
+  app.useGlobalPipes(new ValidationPipe())
 }
