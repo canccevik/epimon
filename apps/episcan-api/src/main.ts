274b858ef0a504b068f5934d09bb276c2ec97d9e
Can Çevik <mazlumcancevik@gmail.com>
2023-11-25 22:55:29 +0300

    feat(esa): add swagger

--- apps/episcan-api/src/main.ts
+++ apps/episcan-api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@modules/app.module'
 import { Config, ENV } from './config'
 import { setupApp } from './setup-app'
+import { setupSwagger } from './setup-swagger'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
@@ -9,6 +10,7 @@ async function bootstrap(): Promise<void> {
   const config = app.get<Config>(ENV)
 
   setupApp(app)
+  setupSwagger(app)
 
   await app.listen(config.PORT)
 }
