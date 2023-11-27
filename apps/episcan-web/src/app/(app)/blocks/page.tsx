17016edf53907b20277dd7c98ca65222e1002722
Can Çevik <mazlumcancevik@gmail.com>
2023-11-28 02:43:44 +0300

    feat(esw): connect blocks page to api

--- apps/episcan-web/src/app/(app)/blocks/page.tsx
+++ apps/episcan-web/src/app/(app)/blocks/page.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@/components/ui/button'
+'use client'
+
+import { Card } from '@/components/ui/card'
 import {
   Table,
   TableBody,
@@ -8,137 +10,103 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table'
-import { ArrowRight } from 'lucide-react'
+import { fetcher, getRelativeTimeFromTimestamp, shortenString } from '@/lib/utils'
+import { Block, Payload } from '@epimon/common'
+import { ArrowRight, LoaderIcon } from 'lucide-react'
+import Link from 'next/link'
+import useSWR from 'swr'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 
 export default function Blocks() {
+  const [page] = useState(1)
+  const blockCount = 10
+
+  const { data, isLoading } = useSWR<Payload<Block[]>>(
+    `/chain?page=${page}&limit=${blockCount}`,
+    fetcher
+  )
+
   return (
     <div className="flex flex-col">
       <h1 className="text-4xl font-semibold tracking-wide">Blocks</h1>
 
       <div className="mt-10">
-        <Table>
-          <TableCaption>
-            <div className="flex gap-x-2 ml-5">
-              <Button className="bg-main-black" size={'sm'}>
-                1
-              </Button>
+        {isLoading ? (
+          <Card className="flex justify-center p-10">
+            <LoaderIcon className="animate-spin" />
+          </Card>
+        ) : (
+          data && (
+            <Table>
+              <TableCaption>
+                <div className="flex gap-x-2 ml-5">
+                  <Button className="bg-main-black" size={'sm'}>
+                    1
+                  </Button>
 
-              <Button className="bg-main-black" size={'sm'}>
-                2
-              </Button>
+                  <Button className="bg-main-black" size={'sm'}>
+                    2
+                  </Button>
 
-              <Button className="bg-main-black" size={'sm'}>
-                3
-              </Button>
+                  <Button className="bg-main-black" size={'sm'}>
+                    3
+                  </Button>
 
-              <Button className="bg-main-black" size={'sm'}>
-                4
-              </Button>
+                  <Button className="bg-main-black" size={'sm'}>
+                    4
+                  </Button>
 
-              <span className="text-2xl font-medium">...</span>
+                  <span className="text-2xl font-medium">...</span>
 
-              <Button className="bg-main-black" size={'sm'}>
-                500
-              </Button>
+                  <Button className="bg-main-black" size={'sm'}>
+                    500
+                  </Button>
 
-              <Button className="bg-main-black" size={'sm'}>
-                <span>Next</span> <ArrowRight size={16} />
-              </Button>
-            </div>
-          </TableCaption>
+                  <Button className="bg-main-black" size={'sm'}>
+                    <span>Next</span> <ArrowRight size={16} />
+                  </Button>
+                </div>
+              </TableCaption>
 
-          <TableHeader>
-            <TableRow>
-              <TableHead>Block</TableHead>
-              <TableHead>Age</TableHead>
-              <TableHead>Txn No.</TableHead>
-              <TableHead>Validated</TableHead>
-              <TableHead>Reward</TableHead>
-            </TableRow>
-          </TableHeader>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Block</TableHead>
+                  <TableHead>Age</TableHead>
+                  <TableHead>Mined by</TableHead>
+                  <TableHead>Reward</TableHead>
+                </TableRow>
+              </TableHeader>
 
-          <TableBody>
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
+              <TableBody>
+                {data &&
+                  data.data?.map((block, i) => {
+                    return (
+                      <TableRow key={i}>
+                        <TableCell className="text-main-blue">
+                          <Link href={`/blocks/${block._id}`}>{block._id}</Link>
+                        </TableCell>
 
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
+                        <TableCell>{getRelativeTimeFromTimestamp(block.timestamp)}</TableCell>
 
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
+                        <TableCell className="text-main-blue">
+                          {block.miner ? (
+                            <Link href={`/address/${block.miner}`}>
+                              {shortenString(block.miner)}
+                            </Link>
+                          ) : (
+                            'System'
+                          )}
+                        </TableCell>
 
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-main-blue">33694860</TableCell>
-              <TableCell>12m ago</TableCell>
-              <TableCell className="text-main-blue">146</TableCell>
-              <TableCell className="text-main-blue">0x3f...cA749aA1</TableCell>
-              <TableCell>0.06795690 EPM</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+                        <TableCell>{block.reward} EPM</TableCell>
+                      </TableRow>
+                    )
+                  })}
+              </TableBody>
+            </Table>
+          )
+        )}
       </div>
     </div>
   )
