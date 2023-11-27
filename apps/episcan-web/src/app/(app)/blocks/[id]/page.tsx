2a663af3df9d4ef5cf19bdcc0080a2aa891f53df
Can Çevik <mazlumcancevik@gmail.com>
2023-11-27 19:46:38 +0300

    feat(esw): connect block detail page to api

--- apps/episcan-web/src/app/(app)/blocks/[id]/page.tsx
+++ apps/episcan-web/src/app/(app)/blocks/[id]/page.tsx
@@ -1,99 +1,120 @@
+'use client'
+
+import { Card } from '@/components/ui/card'
 import { TableRow, TableBody, TableCell, Table } from '@/components/ui/table'
-import { Copy } from 'lucide-react'
+import { fetcher, getRelativeTimeFromTimestamp, shortenString } from '@/lib/utils'
+import { Block, Payload } from '@epimon/common'
+import { Copy, Loader } from 'lucide-react'
+import moment from 'moment'
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+import useSWR from 'swr'
 
 export default function BlockDetail() {
+  const params = useParams()
+  const { data, isLoading } = useSWR<Payload<Block>>(`/chain/${params.id}`, fetcher)
+
   return (
     <div className="flex flex-col">
       <h1 className="text-4xl font-semibold tracking-wide">Block details</h1>
 
       <div className="mt-10">
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell className="text-gray-500">Block ID</TableCell>
-              <TableCell className="flex gap-x-2">
-                <span>655769124363c3efcb13d573</span>
-                <Copy className="cursor-pointer" size={20} />
-              </TableCell>
-            </TableRow>
+        {isLoading ? (
+          <Card className="flex justify-center p-10">
+            <Loader className="animate-spin" />
+          </Card>
+        ) : (
+          data?.data && (
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell className="text-gray-500">Block ID</TableCell>
+                  <TableCell className="flex gap-x-2">
+                    <span>{params.id}</span>
+                    <Copy
+                      className="cursor-pointer"
+                      size={20}
+                      onClick={() => navigator.clipboard.writeText(params.id.toString())}
+                    />
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Timestamp</TableCell>
-              <TableCell className="flex gap-x-1">
-                <span>14m 51s ago</span>
-                <span className="text-gray-500">(Nov-22-2023 04:54:41 PM +UTC)</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Timestamp</TableCell>
+                  <TableCell className="flex gap-x-1">
+                    <span>{getRelativeTimeFromTimestamp(data.data.timestamp)}</span>
+                    <span className="text-gray-500">
+                      ({moment(data.data.timestamp).format('MMM-DD-YYYY hh:mm:ss A [GMT]Z')})
+                    </span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Transactions</TableCell>
-              <TableCell className="flex gap-x-1">
-                <span className="text-main-blue">97 transactions</span>
-                <span>in this block</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Transactions</TableCell>
+                  <TableCell className="flex gap-x-1">
+                    <span className="text-main-blue">
+                      {data.data.transactions.length} transactions
+                    </span>
+                    <span>in this block</span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Validated by</TableCell>
-              <TableCell className="flex flex-col">
-                <span className="text-main-blue">0x70f657164e5b75689b64b7fd1fa275f334f28e18</span>
-                <span>in 5 seconds</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Validated by</TableCell>
+                  <TableCell className="flex flex-col">
+                    <span className="text-main-blue">
+                      <Link href={`/address/${data.data.miner}`}>
+                        {data.data.miner ? shortenString(data.data.miner) : 'System'}
+                      </Link>
+                    </span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Block</TableCell>
-              <TableCell className="flex gap-x-1">
-                <span className="text-main-blue">33720482</span>
-                <span>72 Block Confirmations</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Block reward</TableCell>
+                  <TableCell className="flex">
+                    <span>{data.data.reward} EPM</span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Block reward</TableCell>
-              <TableCell className="flex">
-                <span>7.05 EPM</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Difficulty</TableCell>
+                  <TableCell className="flex">
+                    <span>{data.data.difficulty}</span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Difficulty</TableCell>
-              <TableCell className="flex">
-                <span>5</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Size</TableCell>
+                  <TableCell className="flex">
+                    <span>{new TextEncoder().encode(JSON.stringify(data.data)).length} bytes</span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Size</TableCell>
-              <TableCell className="flex">
-                <span>1,524 bytes</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Hash</TableCell>
+                  <TableCell className="flex">
+                    <span>{data.data.hash}</span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Hash</TableCell>
-              <TableCell className="flex">
-                <span>0x8d0dc3dbba79419165a2c1147bfd2752bd8307476c3dc107a8a2945a4af807c7</span>
-              </TableCell>
-            </TableRow>
+                <TableRow>
+                  <TableCell className="text-gray-500">Parent Hash</TableCell>
+                  <TableCell className="flex">
+                    <span>{data.data.previousBlockHash ?? 'Null'}</span>
+                  </TableCell>
+                </TableRow>
 
-            <TableRow>
-              <TableCell className="text-gray-500">Parent Hash</TableCell>
-              <TableCell className="flex">
-                <span className="text-main-blue">
-                  0x6a120cd635b7d6c3fe1d4d560ced5c8902dff716c635590493f00aba1c63909b
-                </span>
-              </TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="text-gray-500">Nonce</TableCell>
-              <TableCell className="flex">
-                <span>435932</span>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+                <TableRow>
+                  <TableCell className="text-gray-500">Nonce</TableCell>
+                  <TableCell className="flex">
+                    <span>{data.data.nonce}</span>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          )
+        )}
       </div>
     </div>
   )
--- apps/episcan-web/src/lib/utils/index.ts
+++ apps/episcan-web/src/lib/utils/index.ts
@@ -1,14 +1,17 @@
 import { type ClassValue, clsx } from 'clsx'
+import moment from 'moment'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const ROOT_NODE_URI = process.env.NEXT_PUBLIC_ROOT_NODE_URI
+const ROOT_NODE_URI = process.env.NEXT_PUBLIC_EPISCAN_API_URI
 
 export const fetcher = (endpoint: string) =>
   fetch(ROOT_NODE_URI + endpoint).then((res) => res.json())
 
 export const shortenString = (text: string) =>
   text.length >= 20 ? text.slice(0, 5) + '...' + text.slice(text.length - 8, text.length) : text
+
+export const getRelativeTimeFromTimestamp = (timestamp: number) => moment(timestamp).fromNow()
