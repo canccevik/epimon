6ee45546ea7023e96b3fed582b9eb1e98a9657c9
Can Çevik <mazlumcancevik@gmail.com>
2023-11-28 17:21:41 +0300

    feat(esa): add transaction module

--- apps/episcan-api/src/features/features.module.ts
+++ apps/episcan-api/src/features/features.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { BlockchainModule } from './blockchain/blockchain.module'
+import { TransactionModule } from './transaction/transaction.module'
 
 @Module({
-  imports: [BlockchainModule]
+  imports: [BlockchainModule, TransactionModule]
 })
 export class FeaturesModule {}
