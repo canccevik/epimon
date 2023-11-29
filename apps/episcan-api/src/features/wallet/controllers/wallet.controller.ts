974d7d58282ed4a73952a70d35ba59f687c9a516
Can Çevik <mazlumcancevik@gmail.com>
2023-11-29 15:55:19 +0300

    feat(esa): add get balance of wallet action

--- apps/episcan-api/src/features/wallet/controllers/wallet.controller.ts
+++ apps/episcan-api/src/features/wallet/controllers/wallet.controller.ts
@@ -1,6 +1,19 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { Controller, Get, Param } from '@nestjs/common'
+import { ApiTags, ApiParam } from '@nestjs/swagger'
+import { WalletService } from '../services'
+import { Message, WalletAddressDto } from '@epimon/common'
 
 @ApiTags('wallets')
 @Controller('wallets')
-export class WalletController {}
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+
+  @Get(':walletAddress/balance')
+  @ApiParam({ name: 'walletAddress' })
+  @Message('Wallet balance fetched successfully.')
+  public async getBalanceOfAddress(
+    @Param() { walletAddress }: WalletAddressDto
+  ): Promise<{ balance: number }> {
+    return this.walletService.getBalanceOfAddress(walletAddress)
+  }
+}
--- apps/episcan-api/src/features/wallet/services/wallet.service.ts
+++ apps/episcan-api/src/features/wallet/services/wallet.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common'
+import { Config, ENV } from '@config/index'
+import { AXIOS_INSTANCE, Payload } from '@epimon/common'
+import { BadRequestException, HttpStatus, Inject, Injectable } from '@nestjs/common'
+import { Axios } from 'axios'
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+  constructor(
+    @Inject(AXIOS_INSTANCE) private readonly axios: Axios,
+    @Inject(ENV) private readonly config: Config
+  ) {}
+
+  public async getBalanceOfAddress(address: string): Promise<{ balance: number }> {
+    const balanceEndpoint = this.config.ROOT_NODE_URI + `/wallets/${address}/balance`
+    const balanceRequest = await this.axios.get<Payload<{ balance: number }>>(balanceEndpoint)
+
+    if (balanceRequest.status !== HttpStatus.OK) {
+      throw new BadRequestException(balanceRequest.data.message)
+    }
+    return balanceRequest.data.data
+  }
+}
