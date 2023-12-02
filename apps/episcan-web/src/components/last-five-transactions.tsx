3671eb70e98e1fd688c69c2180ca8ed3069fcf0d
Can Çevik <mazlumcancevik@gmail.com>
2023-12-02 23:29:09 +0300

    refactor(esw): refactor last five transactions component

--- apps/episcan-web/src/components/last-five-transactions.tsx
+++ apps/episcan-web/src/components/last-five-transactions.tsx
@@ -1,21 +1,14 @@
 'use client'
 
 import useSWR from 'swr'
-import {
-  Table,
-  TableCaption,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell
-} from './ui/table'
+import { TableRow, TableCell } from './ui/table'
 import { fetcher, shortenString } from '@/lib/utils'
 import { Payload, Transaction } from '@epimon/common'
 import Link from 'next/link'
 import LoaderCard from './loader-card'
 import ErrorCard from './error-card'
 import { appConfig } from '@/config/app'
+import DataTable from './data-table'
 
 export default function LastFiveTransactions() {
   const { data, isLoading, error } = useSWR<Payload<Transaction[]>, Payload<null>>(
@@ -25,59 +18,46 @@ export default function LastFiveTransactions() {
 
   if (error) {
     return <ErrorCard message={error.message} />
-  }
-
-  if (isLoading) {
+  } else if (isLoading) {
     return <LoaderCard />
   }
 
   return (
-    data && (
-      <Table>
-        <TableCaption>
-          <Link href={'/transactions'}>View All Transactions</Link>
-        </TableCaption>
+    data &&
+    data.data && (
+      <DataTable
+        data={data}
+        link={<Link href={'/transactions'}>View All Transactions</Link>}
+        headings={['Transaction', 'From', 'Value']}
+      >
+        {data.data.map((transaction, i) => (
+          <TableRow key={i}>
+            <TableCell className="font-medium">
+              {transaction._id ? (
+                <Link href={`/transactions/${transaction._id}`} className="text-main-blue">
+                  {transaction._id}
+                </Link>
+              ) : (
+                'Null'
+              )}
+            </TableCell>
 
-        <TableHeader>
-          <TableRow>
-            <TableHead>Transaction</TableHead>
-            <TableHead>From</TableHead>
-            <TableHead>Value</TableHead>
+            <TableCell>
+              {transaction.senderAddress ? (
+                <Link href={`/address/${transaction.senderAddress}`} className="text-main-blue">
+                  {shortenString(transaction.senderAddress)}
+                </Link>
+              ) : (
+                'System'
+              )}
+            </TableCell>
+
+            <TableCell>
+              {transaction.amount} {appConfig.coinName}
+            </TableCell>
           </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {data.data?.map((transaction, i) => {
-            return (
-              <TableRow key={i}>
-                <TableCell className="font-medium">
-                  {transaction._id ? (
-                    <Link href={`/transactions/${transaction._id}`} className="text-main-blue">
-                      {transaction._id}
-                    </Link>
-                  ) : (
-                    'Null'
-                  )}
-                </TableCell>
-
-                <TableCell>
-                  {transaction.senderAddress ? (
-                    <Link href={`/address/${transaction.senderAddress}`} className="text-main-blue">
-                      {shortenString(transaction.senderAddress)}
-                    </Link>
-                  ) : (
-                    'System'
-                  )}
-                </TableCell>
-
-                <TableCell>
-                  {transaction.amount} {appConfig.coinName}
-                </TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
+        ))}
+      </DataTable>
     )
   )
 }
