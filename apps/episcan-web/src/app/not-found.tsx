334b140088320a9f46942db3c55a5c632691c18e
Can Çevik <mazlumcancevik@gmail.com>
2023-12-02 20:52:07 +0300

    feat(esw): implement responsive design to not found page

--- apps/episcan-web/src/app/not-found.tsx
+++ apps/episcan-web/src/app/not-found.tsx
@@ -5,19 +5,22 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="text-center flex flex-col gap-y-2 -mt-16">
-        <h1 className="text-[180px] font-semibold tracking-wider">404</h1>
+      <div className="text-center flex flex-col items-center gap-y-2 -mt-16">
+        <h1 className="text-9xl sm:text-[180px] font-semibold tracking-wider">404</h1>
 
-        <div className="mb-5 -mt-10">
+        <div className="mb-5 sm:-mt-5">
           <h2 className="text-3xl font-medium">Page Not Found</h2>
-          <p className="mt-4 text-lg text-gray-700 dark:text-gray-400">
+          <p className="w-f sm:w-full mt-4 text-lg text-gray-700 dark:text-gray-400">
             The page that you are looking for does not exist.
           </p>
         </div>
 
         <Link
           href="/"
-          className={cn(buttonVariants(), 'box-content p-2 flex items-center font-medium')}
+          className={cn(
+            buttonVariants(),
+            'w-9/12 sm:w-full box-content p-2 flex items-center font-medium'
+          )}
         >
           Back to home
         </Link>
