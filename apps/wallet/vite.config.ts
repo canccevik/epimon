b9e3287573fa5f3fb82ae85e82863fb203629f22
Can Çevik <mazlumcancevik@gmail.com>
2023-12-07 19:50:11 +0300

    build(wlt): add hmr feature

--- apps/wallet/vite.config.ts
+++ apps/wallet/vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173
+    }
+  }
 })
