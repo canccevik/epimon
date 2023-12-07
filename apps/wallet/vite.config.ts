e41f36e058547bfd5297b14d8645469bf9474a4b
Can Çevik <mazlumcancevik@gmail.com>
2023-12-07 20:29:57 +0300

    build(wt): add ts paths

--- apps/wallet/vite.config.ts
+++ apps/wallet/vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
 
 export default defineConfig({
   plugins: [react(), crx({ manifest, contentScripts: { injectCss: true } })],
+  resolve: {
+    alias: {
+      '@': root
+    }
+  },
   base: './',
   server: {
     port: 5173,
