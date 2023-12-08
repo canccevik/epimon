7e558f69f11c036d03453f1111e3ca462e2683b7
Can Çevik <mazlumcancevik@gmail.com>
2023-12-08 19:00:42 +0300

    feat(wt): add base components

--- apps/wallet/src/pages/app.tsx
+++ apps/wallet/src/pages/app.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <>
+    <div className="w-[350px] h-[500px] p-5 bg-gray-50 border-gray-50 border-2">
       <h1 className="text-3xl font-bold">Epimon Wallet</h1>
-    </>
+    </div>
   )
 }
